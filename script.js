2c1352d490c2d1b5c3a37012fa0fe65b7b9ed3df
Ana Santos <ana.br@hotmail.com>
2023-03-13 08:38:17 -0300

    atualizações

---
+++ script.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper('.swiper', {
+  cssMode: true,
+  loop: true,
+  navegation: {
+    nextE1: '.swiper-button-next',
+    prevE1: '.swiper-button-prev',
+  },
+  pafination: {
+    el: '.swiper-pagination',
+  },
+  keyboard: true,
+});
+
+console.log('teste');
